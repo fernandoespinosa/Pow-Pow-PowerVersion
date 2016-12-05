5ec15252197ed1037acada02db8203fbdb3865c1
Fernando Espinosa <fernandoespinosa@fernandoespinosa.org>
2016-12-05 09:10:05 -0500

    Testing simplified script...

--- GitHashVersion.ps1
+++ GitHashVersion.ps1
@@ -1,23 +1,2 @@
-$vcsRootId = "%vcs.root.id%";
-Write-Host "<vcsRootId>: $vcsRootId";
-
-$branch = "%teamcity.build.vcs.branch.$vcsRootId%";
-Write-Host "<branch>: $branch";
-
-$branch = $branch.Replace('/', '-');
-Write-Host "<beautified-branch>: $branch";
-
-$hash = "%build.vcs.number.$vcsRootId%";
-Write-Host "<hash>: $hash";
-
-$hash = $hash.Substring(0, 7);
-Write-Host "<beautified-hash>: $hash";
-
-$counter = "%build.counter%";
-Write-Host "<counter>: $counter";
-
-$version = "$counter-$branch.$hash";
-Write-Host "<version>: $version";
-
-Write-Host "##teamcity[setParameter name='version' value='$version']";
-Write-Host "##teamcity[buildNumber '$version']";
+$vcsrootBranch = "%vcsroot.branch%";
+Write-Host "<vcsroot.branch>: $vcsrootBranch";
