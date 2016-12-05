89e8874d3bfdfd6b79c04e0996abf061df53bef4
Fernando Espinosa <fernandoespinosa@fernandoespinosa.org>
2016-12-05 09:19:28 -0500

    Update GitHashVersion.ps1

--- GitHashVersion.ps1
+++ GitHashVersion.ps1
@@ -1,2 +1,6 @@
+
 $vcsrootBranch = "%vcsroot.branch%";
 Write-Host "<vcsroot.branch>: $vcsrootBranch";
+
+Write-Host "<env.VCSROOT_BRANCH>: $env.VCSROOT_BRANCH";
+Write-Host "<env.TEAMCITY_BUILD_BRANCH>: $env.TEAMCITY_BUILD_BRANCH";
