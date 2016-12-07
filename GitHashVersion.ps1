d219a0959074fe7ec1a8ba696b17b3d692b43a39
Fernando Espinosa <fernandoespinosa@fernandoespinosa.org>
2016-12-06 19:18:47 -0500

    Foo was here

--- GitHashVersion.ps1
+++ GitHashVersion.ps1
@@ -3,3 +3,5 @@ Param([string]$powpowBranch, [string]$powpowRevision, [string]$buildCounter);
 Write-Host "<powpowBranch>: $powpowBranch";
 Write-Host "<powpowRevision>: $powpowRevision";
 Write-Host "<buildCounter>: $buildCounter";
+
+# FOO
